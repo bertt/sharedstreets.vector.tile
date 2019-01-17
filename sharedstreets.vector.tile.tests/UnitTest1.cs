9b78157c32b7753becfde969c15e44c0f3998775
bert <btemme@hotmail.com>
2019-01-17 17:29:18 +0100

    adding testfiles

--- sharedstreets.vector.tile.tests/UnitTest1.cs
+++ sharedstreets.vector.tile.tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using Sharedstreets.Vector.Tile;
+using System.IO;
 
 namespace Tests
 {
@@ -8,8 +9,16 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            Assert.IsTrue(new Class1().Add(5, 6) == 11);
-            Assert.Pass();
+            var geometryStream = File.OpenRead("./testfixtures/12-1171-1566.geometry.6.pbf");
+            var intersectionStream = File.OpenRead("./testfixtures/12-1171-1566.intersection.6.pbf");
+            var metadataStream = File.OpenRead("./testfixtures/12-1171-1566.metadata.6.pbf");
+            var referenceStream = File.OpenRead("./testfixtures/12-1171-1566.reference.6.pbf");
+
+
+            SharedStreetsTileParser.Parse(geometryStream);
+            // geometryStream.
+
+
         }
     }
 }
---
+++ sharedstreets.vector.tile/SharedStreetsTileParser.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Sharedstreets.Vector.Tile
+{
+    public class SharedStreetsTileParser
+    {
+        public static void Parse(Stream stream)
+        {
+
+        }
+    }
+}
