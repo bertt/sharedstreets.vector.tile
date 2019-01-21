5144eacdeacfea664f884cac365c673a9b26bce6
bert <btemme@hotmail.com>
2019-01-21 15:54:58 +0100

    got something parsing

--- sharedstreets.vector.tile.tests/UnitTest1.cs
+++ sharedstreets.vector.tile.tests/UnitTest1.cs
@@ -9,16 +9,25 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            var geometryStream = File.OpenRead("./testfixtures/12-1171-1566.geometry.6.pbf");
-            var intersectionStream = File.OpenRead("./testfixtures/12-1171-1566.intersection.6.pbf");
-            var metadataStream = File.OpenRead("./testfixtures/12-1171-1566.metadata.6.pbf");
-            var referenceStream = File.OpenRead("./testfixtures/12-1171-1566.reference.6.pbf");
+            // sample amsterdam geometry url: 
+            // https://tiles.sharedstreets.io/osm/planet-181224/12-2103-1346.geometry.6.pbf
+            // https://tiles.sharedstreets.io/osm/planet-181224/12-2103-1346.intersection.6.pbf
+            // https://tiles.sharedstreets.io/osm/planet-181224/12-2103-1346.metadata.6.pbf
+            // https://tiles.sharedstreets.io/osm/planet-181224/12-2103-1346.reference.6.pbf
+            var amsterdamTile = "./testfixtures/12-2103-1346.";
+            var geometryStream = File.OpenRead(amsterdamTile + "geometry.6.pbf");
+            var intersectionStream = File.OpenRead(amsterdamTile + "intersection.6.pbf");
+            var metadataStream = File.OpenRead(amsterdamTile + "metadata.6.pbf");
+            var referenceStream = File.OpenRead(amsterdamTile + "reference.6.pbf");
 
-
-            SharedStreetsTileParser.Parse(geometryStream);
-            // geometryStream.
-
-
+            var geometry = SharedStreetsParser.ParseGeometry(geometryStream);
+            Assert.IsTrue(geometry.Lonlats.Count == 50906);
+            var intersection = SharedStreetsParser.ParseIntersection(intersectionStream);
+            Assert.IsTrue(intersection.Id == "7f1e47e658e554625c51c6fc6cdafcda");
+            var metadata = SharedStreetsParser.ParseMetadata(metadataStream);
+            Assert.IsTrue(metadata.OsmMetadata.WaySections.Count == 8431);
+            var reference = SharedStreetsParser.ParseReference(referenceStream);
+            Assert.IsTrue(reference.LocationReferences.Count == 17382);
         }
     }
 }
--- sharedstreets.vector.tile/SharedStreetsTileParser.cs
+++ sharedstreets.vector.tile/SharedStreetsTileParser.cs
@@ -2,11 +2,30 @@
 
 namespace Sharedstreets.Vector.Tile
 {
-    public class SharedStreetsTileParser
+    public static class SharedStreetsParser
     {
-        public static void Parse(Stream stream)
+        public static SharedStreetsGeometry ParseGeometry(Stream stream)
         {
+            var sharedStreetsGeometry = SharedStreetsGeometry.Parser.ParseFrom(stream);
+            return sharedStreetsGeometry;
+        }
 
+        public static SharedStreetsMetadata ParseMetadata(Stream stream)
+        {
+            var sharedStreetsMetadata = SharedStreetsMetadata.Parser.ParseFrom(stream);
+            return sharedStreetsMetadata;
+        }
+
+        public static SharedStreetsIntersection ParseIntersection(Stream stream)
+        {
+            var sharedStreetsIntersection = SharedStreetsIntersection.Parser.ParseFrom(stream);
+            return sharedStreetsIntersection;
+        }
+
+        public static SharedStreetsReference ParseReference(Stream stream)
+        {
+            var sharedStreetsReference= SharedStreetsReference.Parser.ParseFrom(stream);
+            return sharedStreetsReference;
         }
     }
 }
