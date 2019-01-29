baf9672e99e5688fc02eb5c6c730f07aa0555455
bert <btemme@hotmail.com>
2019-01-29 13:38:55 +0100

    cleaning

--- sharedstreets.vector.tile.tests/PbfParsingTests.cs
+++ sharedstreets.vector.tile.tests/PbfParsingTests.cs
@@ -25,19 +25,5 @@ namespace Sharedstreets.Vector.Tile.Tests
             var lonlats = geometries[0].Lonlats;
             Assert.IsTrue(SharedStreets.GeometryId(lonlats) == geometries[0].Id);
         }
-
-        [Test]
-        public void ProtobufRoundTest()
-        {
-            var amsterdamTile = "./testfixtures/12-2103-1346.";
-            var geometryStream = File.OpenRead(amsterdamTile + "geometry.6.pbf");
-            var geometries = SharedStreetsTileParser.Parse<SharedStreetsGeometry>(geometryStream);
-            Assert.IsTrue(geometries.Count == 6202);
-            var stream = SharedStreetsTileWriter.Write(geometries);
-            Assert.IsTrue(geometryStream.Length == stream.Length);
-            stream.Position = 0;
-            var newgeometries = SharedStreetsTileParser.Parse<SharedStreetsGeometry>(stream);
-            Assert.IsTrue(newgeometries.Count == 6202);
-        }
     }
 }
